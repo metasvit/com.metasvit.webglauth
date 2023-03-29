2ca8b515894a800ef7a7f1f17f0baef43928627b
M. K <maksym@metasvit.io>
2023-03-29 16:31:06 +0300

    fix bugs with address not being updated and examples

--- Runtime/MetamaskController.cs
+++ Runtime/MetamaskController.cs
@@ -1,4 +1,4 @@
-#undef DEBUG
+// #undef UNITY_EDITOR
 using System;
 using System.Numerics;
 using Nethereum.Hex.HexTypes;
@@ -7,48 +7,61 @@ using UnityEngine;
 
 namespace WebGlAuth
 {
-    public class MetamaskController : MonoBehaviour, IMetamaskController
+    public class MetamaskController : MonoBehaviour
     {
-        public static IMetamaskController Instance { get; private set; }
+        public static bool IsMetamaskInitialised { get; private set; }
+        public static string SelectedAccountAddress { get; private set; }
+        public static BigInteger CurrentChainId { get; private set; }
+
+        public static event Action<string> NewAccountSelectedEvent;
+        public static event Action<BigInteger> ChainChangedEvent;
+        public static event Action<string> ErrorEvent;
+
+        private static MetamaskController _instance;
 
         [SerializeField] private bool _autoConnectOnStart = true;
 
-        public bool IsMetamaskInitialised { get; private set; }
-
-        public string SelectedAccountAddress { get; private set; }
-        public BigInteger CurrentChainId { get; private set; }
-
-        public event Action<string> NewAccountSelectedEvent;
-        public event Action<BigInteger> ChainChangedEvent;
-        public event Action<string> ErrorEvent;
-
-        private void Awake()
+        private void Start()
         {
-            if (Instance != null)
+            // Debug.Log("MetamaskController.Start()");
+            if (_instance != null)
             {
                 Destroy(gameObject);
                 return;
             }
 
             var instance = FindAnyObjectByType<MetamaskController>(FindObjectsInactive.Include);
-            Instance = instance;
+            _instance = instance;
             DontDestroyOnLoad(gameObject);
             
             if (_autoConnectOnStart)
             {
-                TryEnableEthereum();
+                TryEnableEthereumInternal();
             }
         }
 
-        public void TryEnableEthereum()
+        public static void TryEnableEthereum()
         {
+            if (_instance != null)
+            {
+                _instance.TryEnableEthereumInternal();
+            }
+        }
+
+        private void TryEnableEthereumInternal()
+        {
+            // Debug.Log("MetamaskController.TryEnableEthereum()");
 #if !UNITY_EDITOR
             if (MetamaskWebglInterop.IsMetamaskAvailable())
             {
                 if (!IsMetamaskInitialised)
                 {
+                    // Debug.Log("MetamaskController.TryEnableEthereum() - EnableEthereum");
                     MetamaskWebglInterop.EnableEthereum(gameObject.name, nameof(EthereumEnabled), nameof(OnError));
+                    return;
                 }
+
+                // Debug.Log("MetamaskController.TryEnableEthereum() - isMetamaskInitialised = true");
             }
             else
             {
@@ -57,34 +70,55 @@ namespace WebGlAuth
 #endif
         }
 
-        private void EthereumEnabled(string addressSelected)
+        /// <summary>
+        /// For internal use only!
+        /// </summary>
+        public void EthereumEnabled(string addressSelected)
         {
+            // Debug.Log($"MetamaskController.EthereumEnabled({addressSelected})");
 #if !UNITY_EDITOR
             if (!IsMetamaskInitialised)
             {
+                // Debug.Log($"MetamaskController.EthereumEnabled() - EthereumInit");
                 MetamaskWebglInterop.EthereumInit(gameObject.name, nameof(NewAccountSelected), nameof(ChainChanged));
+                // Debug.Log($"MetamaskController.EthereumEnabled() IsMetamaskInitialised = true");
                 IsMetamaskInitialised = true;
-                return;
             }
 
             NewAccountSelected(addressSelected);
 #endif
         }
 
-        private void ChainChanged(string chainId)
+        /// <summary>
+        /// For internal use only!
+        /// </summary>
+        public void ChainChanged(string chainId)
         {
+            // Debug.Log($"MetamaskController.ChainChanged({chainId})");
             CurrentChainId = new HexBigInteger(chainId).Value;
             ChainChangedEvent?.Invoke(CurrentChainId);
         }
 
-        private void NewAccountSelected(string accountAddress)
+        /// <summary>
+        /// For internal use only!
+        /// </summary>
+        public void NewAccountSelected(string accountAddress)
         {
+            // Debug.Log($"MetamaskController.NewAccountSelected({accountAddress})");
             SelectedAccountAddress = accountAddress;
             NewAccountSelectedEvent?.Invoke(SelectedAccountAddress);
+#if !UNITY_EDITOR
+            // Debug.Log($"MetamaskController.NewAccountSelected() - GetChainId");            
+            MetamaskWebglInterop.GetChainId(gameObject.name, nameof(ChainChanged), nameof(OnError));
+#endif
         }
 
-        private void OnError(string errorMessage)
+        /// <summary>
+        /// For internal use only!
+        /// </summary>
+        public void OnError(string errorMessage)
         {
+            // Debug.Log($"MetamaskController.OnError({errorMessage})");
             ErrorEvent?.Invoke(errorMessage);
         }
     }
--- Samples~/Example/WebGlAuthDemo.cs
+++ Samples~/Example/WebGlAuthDemo.cs
@@ -1,6 +1,7 @@
 using System.Numerics;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace WebGlAuth.Example
 {
@@ -9,6 +10,7 @@ namespace WebGlAuth.Example
         [SerializeField] TMP_InputField _addressText;
         [SerializeField] TMP_InputField _chainIdText;
         [SerializeField] TMP_InputField _errorText;
+        [SerializeField] Button _tryEnableEthereumButton;
 
         private string _address;
         private BigInteger _chainId;
@@ -16,15 +18,43 @@ namespace WebGlAuth.Example
 
         void Start()
         {
-            MetamaskController.Instance.NewAccountSelectedEvent += OnNewAccountSelectedEvent;
-            MetamaskController.Instance.ChainChangedEvent += OnChainChangedEvent;
-            MetamaskController.Instance.ErrorEvent += OnErrorEvent;
+            Debug.Log("WebGlAuthDemo.Start()");
+            _tryEnableEthereumButton.onClick.AddListener(TryEnableEthereum);
+            MetamaskController.NewAccountSelectedEvent += OnNewAccountSelectedEvent;
+            MetamaskController.ChainChangedEvent += OnChainChangedEvent;
+            MetamaskController.ErrorEvent += OnErrorEvent;
+            if (!MetamaskController.IsMetamaskInitialised)
+            {
+                MetamaskController.TryEnableEthereum();
+            }
+            else
+            {
+                _address = MetamaskController.SelectedAccountAddress;
+                _chainId = MetamaskController.CurrentChainId;
+            }
+
             UpdateView();
+        }
+
+        void OnDestroy()
+        {
+            Debug.Log("WebGlAuthDemo.OnDestroy()");
+            _tryEnableEthereumButton.onClick.RemoveAllListeners();
+            MetamaskController.NewAccountSelectedEvent -= OnNewAccountSelectedEvent;
+            MetamaskController.ChainChangedEvent -= OnChainChangedEvent;
+            MetamaskController.ErrorEvent -= OnErrorEvent;
+        }
+
+        private void TryEnableEthereum()
+        {
+            Debug.Log("WebGlAuthDemo.TryEnableEthereum()");
+            MetamaskController.TryEnableEthereum();
         }
 
         private void UpdateView()
         {
-            if (MetamaskController.Instance.IsMetamaskInitialised)
+            Debug.Log($"WebGlAuthDemo.UpdateView(): MetamaskController.IsMetamaskInitialised = {MetamaskController.IsMetamaskInitialised}");
+            if (MetamaskController.IsMetamaskInitialised)
             {
                 _addressText.text = _address;
                 _chainIdText.text = _chainId.ToString();
@@ -41,6 +71,7 @@ namespace WebGlAuth.Example
 
         private void OnNewAccountSelectedEvent(string address)
         {
+            Debug.Log($"WebGlAuthDemo.OnNewAccountSelectedEvent({address})");
             _address = address;
             _lastError = string.Empty;
             UpdateView();
@@ -48,6 +79,7 @@ namespace WebGlAuth.Example
 
         private void OnChainChangedEvent(BigInteger chainId)
         {
+            Debug.Log($"WebGlAuthDemo.OnChainChangedEvent({chainId})");
             _chainId = chainId;
             _lastError = string.Empty;
             UpdateView();
@@ -55,6 +87,7 @@ namespace WebGlAuth.Example
 
         private void OnErrorEvent(string error)
         {
+            Debug.Log($"WebGlAuthDemo.OnErrorEvent({error})");
             _lastError = error;
             UpdateView();
         }
